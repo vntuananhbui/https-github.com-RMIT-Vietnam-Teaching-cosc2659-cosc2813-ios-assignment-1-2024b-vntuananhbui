89c1a481fad9d8381a8c95a2eb87cfe8506ccbd2
vntuananh <vntuananhbui@gmail.com>
2024-07-20 01:11:23 +0700

    Initial Commit

---
+++ LuxCar_S3970375_ASM1_COSC2659/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LuxCar_S3970375_ASM1_COSC2659
+//
+//  Created by Tuan Anh Bui on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LuxCar_S3970375_ASM1_COSC2659/LuxCar_S3970375_ASM1_COSC2659App.swift
@@ -0,0 +1,17 @@
+//
+//  LuxCar_S3970375_ASM1_COSC2659App.swift
+//  LuxCar_S3970375_ASM1_COSC2659
+//
+//  Created by Tuan Anh Bui on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LuxCar_S3970375_ASM1_COSC2659App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
